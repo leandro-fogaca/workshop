b7c33f5bcf5f0df77f543f42c015a893da25268f
leandro-fogaca <leandro.fogaca87@gmail.com>
2021-09-15 09:37:11 -0300

    Service layer, component registration

--- src/main/java/com/moufog/workshop/repositories/UserRepository.java
+++ src/main/java/com/moufog/workshop/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.moufog.workshop.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.moufog.workshop.entities.User;
 
