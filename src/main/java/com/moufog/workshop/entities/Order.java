5864e2387068b3070f04bee1d27e5d123d2f145a
leandro-fogaca <leandro.fogaca87@gmail.com>
2021-09-15 16:02:48 -0300

    OrderStatus enum

--- src/main/java/com/moufog/workshop/entities/Order.java
+++ src/main/java/com/moufog/workshop/entities/Order.java
@@ -13,7 +13,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import com.moufog.workshop.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order") // Renomeando a tabela para evitar duplicidade de nomes
@@ -23,10 +23,12 @@ public class Order implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
+	private Integer orderStatus;
+
 	@ManyToOne // Indica ao JPA que é uma chave estrangeira
 	@JoinColumn(name = "client_id") // Criado um nome para a chave estrangeira
 	private User client;
@@ -34,10 +36,11 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -55,6 +58,16 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus); // Converteu o O.S de int para OrderStatus
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
