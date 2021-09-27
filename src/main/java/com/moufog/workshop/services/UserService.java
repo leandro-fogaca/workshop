fc328f68ffc98e99ccba26552177c6a2270b8362
leandro-fogaca <leandro.fogaca87@gmail.com>
2021-09-27 14:05:54 -0300

    Exception handling - update

--- src/main/java/com/moufog/workshop/services/UserService.java
+++ src/main/java/com/moufog/workshop/services/UserService.java
@@ -3,11 +3,16 @@ package com.moufog.workshop.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.moufog.workshop.entities.User;
 import com.moufog.workshop.repositories.UserRepository;
+import com.moufog.workshop.services.exceptions.DatabaseException;
 import com.moufog.workshop.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,14 +35,23 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getOne(id);
 		updateData(entity, obj);
 		return repository.save(entity);
-
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
